49621613ea1daea08d2e2f59e87c2f074df72c54
ShlomoNus <shlomonuswork@gmail.com>
2025-08-18 11:13:38 +0000

    Bump version to 1.0.15 in package.json and clean up type exports in general.ts

--- src/general.ts
+++ src/general.ts
@@ -3,16 +3,6 @@ export type AnyType = any;
 
 export type Primitives = string | boolean | number | null | undefined;
 
-export type Success<TPayload = undefined> = {
-    status: true;
-    payload: TPayload;
-    statusCode: number;
-};
-
-export type Failed = { status: false; message: string; statusCode: number };
-
-export type Result<TData> = Success<TData> | Failed;
-
 export type Optional<TData> = TData | null | undefined;
 
 export type Pritter<T> = {
